bf21516ee10f44aa1e98ae75f42343d1d8ade3d4
Mary Angel <201910994@fit.edu.ph>
2023-08-23 18:32:44 +0800

    task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function FilterProductTable ({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+    
+    return (
+        <div className="flex flex-col w-[15%] m-5">
+            <SearchBar 
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly} 
+            />
+            <ProductTable 
+                products={products} 
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+            />
+        </div>
+    );
 }
 
-export default App
+function ProductCategoryRow ({ category }) {;
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+function ProductRow ({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>
+            {product.name}
+        </span>
+
+    return (
+        <tr >
+            <td >{name}</td>
+            <td >{product.price}</td>
+        </tr>
+    );
+}
+
+function ProductTable ({ products, filterText, inStockOnly }) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if ( product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+            return;
+        }
+        if(inStockOnly && !product.stocked) {
+            return;
+        }
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category}
+                />
+            );
+        }
+
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name}
+            />
+        );
+
+        lastCategory = product.category;
+    });
+
+    return (
+        <table >
+            <thead >
+                <tr >
+                    <th >Name</th>
+                    <th className="text-left">Price</th>
+                </tr>
+            </thead>
+
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function SearchBar( {filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange} ) {
+    return (
+        <form>
+            <input 
+                type="text" 
+                value={filterText}
+                placeholder="Search..." 
+                onChange={(e) => onFilterTextChange(e.target.value)}
+                className="w-3/4 px-2 border border-black" 
+            />
+            <label className="flex gap-2 mx-2">
+                <input 
+                    type="checkbox" 
+                    checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)}
+                />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App () {
+    return <FilterProductTable products={PRODUCTS} />
+}
